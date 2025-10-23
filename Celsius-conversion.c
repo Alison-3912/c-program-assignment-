6d1536a914360a7618a8ce0cea01bf047e54df36
Alison-3912 <alisonbundi61@gmail.com>
2025-10-23 10:28:27 +0300

    Add files via upload

---
+++ Celsius-conversion.c
@@ -0,0 +1,18 @@
+//Name:Alisbundi 
+//RegNo:CT101/G/26552/25
+#include <stdio.h>
+
+float convertToCelsius(float fahrenheit)
+ {
+    return (fahrenheit - 32) * 5.0 / 9.0;
+}
+
+int main() {
+    float fahrenheit;
+    
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+    
+    printf("Temperature in Celsius = %.2f°C\n", convertToCelsius(fahrenheit));
+    return 0;
+}
